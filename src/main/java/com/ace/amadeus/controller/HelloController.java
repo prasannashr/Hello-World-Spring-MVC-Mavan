617cb4e16c636835c2c8aa54765e9808de2ac861
prasanna <prasanna.shrestha@aceit.com.np>
2013-04-21 18:04:32 +0545

    Hello world in spring mvc using maven

---
+++ src/main/java/com/ace/amadeus/controller/HelloController.java
@@ -0,0 +1,27 @@
+package com.ace.amadeus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+
+public class HelloController {
+
+	@RequestMapping(method = RequestMethod.GET, value = "abc")
+	public String printWelcome(ModelMap model) {
+
+		model.addAttribute("message", "Hurry hihihihih:)");
+		return "hello";
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "cde")
+	public String printWelcome2(ModelMap model) {
+
+		model.addAttribute("message", "Hello CDE");
+		return "hello";
+
+	}
+	
+}
